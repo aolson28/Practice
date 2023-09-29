2c1754bade404115a267299ade818624c359f625
aolson28 <aaronmichael.olson@gmail.com>
2023-09-28 21:24:43 -0400

    9.28.23 making new column

---
+++ 9.28.23_odd_vs_even.sql
@@ -0,0 +1,12 @@
+Select number,
+case when number % 2 = 0 then "Even"
+else "Odd"
+END
+AS "is_even"
+FROM numbers
+--Paramaters numbers--
+--Retruns Even or odd strings--
+--Example if 2, say "Even"; if 3, say "Odd"--
+--Pseudo does the number column exist? From numbers table? column number? Use modulo to return "Even" when %2=0 or else return odd?--
+
+
