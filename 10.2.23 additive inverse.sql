e2d7ce64972a9fd02ab8af4f55202d8589d31337
aolson28 <aaronmichael.olson@gmail.com>
2023-10-02 14:18:30 -0400

    Create 10.2.23 additive inverse.sql

---
+++ 10.2.23 additive inverse.sql
@@ -0,0 +1,7 @@
+SELECT (number * -1) AS res
+FROM opposite;
+
+--p opposite value of integer or float--
+--r res column with integer or float of opposite of given value--
+--e -5 should return 5--
+--p multiply value with -1?--
