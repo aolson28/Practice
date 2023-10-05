b2431a04d800e6c12f5cb9a4f00574ebcbdfd615
aolson28 <aaronmichael.olson@gmail.com>
2023-10-05 16:07:20 -0400

    Create 10.5.23 number of milliseconds.sql

---
+++ 10.5.23 number of milliseconds.sql
@@ -0,0 +1,6 @@
+select (h * 60 * 60 * 1000) + (m * 60 * 1000) + (s * 1000) as res from past
+
+--p number
+--r number of milliseconds after midnight
+--e 12:01:00 am would be 60000 ms
+--p Sum of ms, each 
