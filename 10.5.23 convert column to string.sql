725d6ea704683b014d0376cac6590fa50c11f0c7
aolson28 <aaronmichael.olson@gmail.com>
2023-10-05 22:25:43 -0400

    Create 10.5.23 convert column to string.sql

---
+++ 10.5.23 convert column to string.sql
@@ -0,0 +1,7 @@
+SELECT CONCAT ('Hello, ', name, ' how are you doing today?') AS greeting
+FROM person;
+
+--p string
+--r return string "Hello, " & name & " how are you doing today?"
+--e "Hello, Aaron how are you doing today?"
+--p use SELECT CONCAT
