ce80c7682406f7310151dd64e3851e1d6d7de7a2
aolson28 <aaronmichael.olson@gmail.com>
2023-10-05 22:06:55 -0400

    Create 10.5.23 INSERT INTO participants.sql

---
+++ 10.5.23 INSERT INTO participants.sql
@@ -0,0 +1,9 @@
+INSERT INTO participants
+VALUES ('Aaron',28, true);
+
+SELECT * FROM participants;
+
+--p insert string, integer, and bool
+--r table with name added to list
+--e Aaron, 28, true - attending
+--p Use INSERT INTO. 
