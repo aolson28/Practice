7362e4ac39e66f8ed837303eadf385ab24d5484e
aolson28 <aaronmichael.olson@gmail.com>
2023-10-06 16:02:22 -0400

    Create 10.6.23 add goals.sql

---
+++ 10.6.23 add goals.sql
@@ -0,0 +1,8 @@
+Select la_liga_goals + copa_del_rey_goals + champions_league_goals AS res
+FROM goals;
+
+--p integer
+--r sum of all 3 columns
+--e 1 + 2 + 1 =4
+--p each column added AS res
+-- you will be given a table, goals, with columns la_liga_goals, copa_del_rey_goals, and champions_league_goals. Return a table with a column, res.
