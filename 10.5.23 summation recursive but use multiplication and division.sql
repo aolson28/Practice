e98e713013be0b75713c3a6cbd282bd280f34e17
aolson28 <aaronmichael.olson@gmail.com>
2023-10-05 22:54:45 -0400

    Create 10.5.23 summation recursive but use multiplication and division.sql

---
+++ 10.5.23 summation recursive but use multiplication and division.sql
@@ -0,0 +1,7 @@
+SELECT n, n*(n+1)/2 AS res 
+FROM kata;
+
+--p integer
+--r the summation of all the digits
+--e 3 -> 1 + 2 + 3 = 6
+--p could use averages and multiply instead of adding each value. 0 to n = n+1. Multiply n * (n+1)/2
