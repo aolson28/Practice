af3b8db29fc6a8c4c9ce5b21169b69bbd7f88e79
aolson28 <aaronmichael.olson@gmail.com>
2023-10-07 15:33:45 -0400

    Create 10.7.23 repeat and reverse functions_alt method.sql

---
+++ 10.7.23 repeat and reverse functions_alt method.sql
@@ -0,0 +1,9 @@
+SElECT REPEAT(name,3) AS name, REVERSE(characteristics) AS characteristics
+FROM monsters;
+
+--P List
+--R name is string concat 3 times, characteristics string is reversed
+--E Sam, SillyWilly -> SamSamSam, ylliWylliS
+--P use CONCAT() for name and REVERSE() for characteristics
+
+
