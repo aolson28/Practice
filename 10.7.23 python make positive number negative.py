6b3217533018d0c7fec70efc67a237b78d6f455d
aolson28 <aaronmichael.olson@gmail.com>
2023-10-07 16:56:37 -0400

    Create 10.7.23 python make positive number negative.py

---
+++ 10.7.23 python make positive number negative.py
@@ -0,0 +1,10 @@
+def make_negative( number ):
+    if number > 0:
+        return number * -1
+    else:
+        return number
+
+# P integer
+# R show the negative of a given number, ignoring currently negative numbers
+# E 5 -> -5
+# P use if and else. if number > 0 excludes all values not to be changed
