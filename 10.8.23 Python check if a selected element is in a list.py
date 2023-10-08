af5665935db0676998dc10e226a95a3d465d100d
aolson28 <aaronmichael.olson@gmail.com>
2023-10-08 16:29:44 -0400

    Create 10.8.23 Python check if a selected element is in a list.py

---
+++ 10.8.23 Python check if a selected element is in a list.py
@@ -0,0 +1,7 @@
+def check(a, x):
+    return x in a
+
+# P bool
+# R retrun true if a contains x and false if a does not contain x
+# E a = [1, -1, 2, -2, 3, c, 'test'] b = 'test'-> True
+# P the seq a and elem x were not in the function, use x in a to give the bool
