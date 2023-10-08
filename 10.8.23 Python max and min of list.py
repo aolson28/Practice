c3f521e0f74292bb92ce07a9b2217222d3223423
aolson28 <aaronmichael.olson@gmail.com>
2023-10-08 16:29:49 -0400

    Create 10.8.23 Python max and min of list.py

---
+++ 10.8.23 Python max and min of list.py
@@ -0,0 +1,10 @@
+def minimum(arr):
+    return min(arr)
+
+def maximum(arr):
+    return max(arr)
+
+# P integers
+# R retrun max of list and min of list
+# E [1, -1, 2, -2, 3] -> max is 3, min is -2
+# P return min(arr) for minimum and return max(arr) for maximum
