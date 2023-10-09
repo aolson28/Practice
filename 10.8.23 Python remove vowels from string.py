c3c91f7334a11f99efe69dd0d1984527a8caa87e
aolson28 <aaronmichael.olson@gmail.com>
2023-10-08 22:49:24 -0400

    Create 10.8.23 Python remove vowels from string.py

---
+++ 10.8.23 Python remove vowels from string.py
@@ -0,0 +1,22 @@
+def disemvowel(string_):
+    repa = string_.replace('a','')
+    repe = repa.replace('e','')
+    repi = repe.replace('i','')
+    repo = repi.replace('o','')
+    repu = repo.replace('u','')
+    repA = repu.replace('A','')
+    repE = repA.replace('E','')
+    repI = repE.replace('I','')
+    repO = repI.replace('O','')
+    repU = repO.replace('U','')
+    return repU
+
+# P string
+# R string without vowels
+# E 'TEST ABCDEF' -> 'TST BCDF'
+# P could use stacked replaces like above. Should use below
+#
+# def disemvowel(string_):
+#    for i in 'aeiouAEIOU':
+#       string_ = string_.replace(i,'')
+#    return string_
