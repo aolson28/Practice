d0d920b4efd1e3601169ee3dbb6a440a15505990
aolson28 <aaronmichael.olson@gmail.com>
2023-10-10 19:56:23 -0400

    Create 10.10.23 concat list into string.py

---
+++ 10.10.23 concat list into string.py
@@ -0,0 +1,8 @@
+def smash(words):
+    return ' '.join(words)
+    
+    
+# P string
+# R take words in a list and turn them into a string with words separated by spaces
+# E ['up', 'dog', 'what's', 'that']  =>  'up dog what's that'
+# P ' '.join(list)
