8f6ebb7e7df752eaf634016d8fa8f9aa666611a4
aolson28 <aaronmichael.olson@gmail.com>
2023-10-10 22:54:10 -0400

    Update 10.10.23 sum of the sequence.py

--- 10.10.23 sum of the sequence.py
+++ 10.10.23 sum of the sequence.py
@@ -1,10 +1,18 @@
+# def series_sum(n):
+#    s = 1
+#    d = 0
+#    for i in range(0,n):
+#        d = d + 1/s
+#        s += 3
+#    return "%0.2f" % d
+#
+# simplified below
+
 def series_sum(n):
-    s = 1
-    d = 0
-    for i in range(0,n):
-        d = d + 1/s
-        s += 3
-    return "%0.2f" % d
+    s = 0
+    for i in range(n):
+        s += 1/(1 + i * 3)
+    return "%0.2f" % s 
     
 # P string of number with 2 decimal places
 # R sum of the series to nth increment where series is 1/1 -> 1/4 (bottom increases by 3)
