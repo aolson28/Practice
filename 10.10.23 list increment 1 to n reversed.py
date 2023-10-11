c71bf161aa701ca311e7d1856dbc8830f7023ee0
aolson28 <aaronmichael.olson@gmail.com>
2023-10-10 21:11:41 -0400

    Create 10.10.23 list increment 1 to n reversed.py

---
+++ 10.10.23 list increment 1 to n reversed.py
@@ -0,0 +1,7 @@
+def reverse_seq(n):
+    return [i + 1 for i in range(0,n)][::-1]
+    
+# P List
+# R list with n number of items, increment by 1 from 1-n, in reversed order
+# E 3 = [3,2,1]
+# P use for loop to increment, index [::-1] to reverse the order
