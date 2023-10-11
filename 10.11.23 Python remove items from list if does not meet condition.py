562f1b800626b2e87ff39d0a29c00a902138148c
aolson28 <aaronmichael.olson@gmail.com>
2023-10-11 15:48:12 -0400

    Create 10.11.23 Python remove items from list if does not meet condition.py

---
+++ 10.11.23 Python remove items from list if does not meet condition.py
@@ -0,0 +1,7 @@
+def friend(x):
+    return [i for i in x if len(i) == 4]
+
+# P list
+# R list with only strings with length of 4
+# E ['Yes','True','Falser'] -> ['True']
+# P list with strings, looped for condition == 4
