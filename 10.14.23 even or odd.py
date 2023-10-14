66b6ce70ecc91cf70beda159c650e7faa6dc9d23
aolson28 <aaronmichael.olson@gmail.com>
2023-10-14 15:26:34 -0400

    Create 10.14.23 even or odd.py

---
+++ 10.14.23 even or odd.py
@@ -0,0 +1,9 @@
+def even_or_odd(number):
+    if number % 2 == 0:
+        return 'Even'
+    else:
+        return 'Odd'
+# P str
+# R str saying if the number is odd or even
+# E 3 -> 'Odd' and 2 -> 'Even'
+# P if else, determine odd by number % 2 == 0
