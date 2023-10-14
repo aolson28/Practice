424b907b093ac988bf8cfed3e2d59042eae3b8d8
aolson28 <aaronmichael.olson@gmail.com>
2023-10-14 15:36:57 -0400

    Create 10.14.23 string replaced for bool.py

---
+++ 10.14.23 string replaced for bool.py
@@ -0,0 +1,7 @@
+def bool_to_word(boolean):
+    return "Yes" if boolean else "No"
+
+# P str
+# R str "Yes" for bool true, "No" for bool false
+# true -> "Yes"
+# P return "Yes" if boolean else "No"
