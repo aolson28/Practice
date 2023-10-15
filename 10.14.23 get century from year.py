48d18617402249131a062b0310dc495aa1c0a869
aolson28 <aaronmichael.olson@gmail.com>
2023-10-14 21:18:31 -0400

    Create 10.14.23 get century from year.py

---
+++ 10.14.23 get century from year.py
@@ -0,0 +1,7 @@
+def century(year):
+    return int((year- 1)/100)+1
+
+# P int
+# R int representing the century
+# E 1900 -> 19, 1901 -> 20
+# P subtract 1 year, divide by 100, and then add 1 to century
