b08d620523ebed2ad9a1ca9dcbaa4639a5086ea5
aolson28 <aaronmichael.olson@gmail.com>
2023-10-14 21:01:12 -0400

    Create 10.14.23 put the value into a string.py

---
+++ 10.14.23 put the value into a string.py
@@ -0,0 +1,7 @@
+def greet(name):
+    return 'Hello, ' + name + ' how are you doing today?'
+
+# P str
+# R str with the value passed into it
+# E A A Ron -> 'Hello, A A Ron how are you doing today?'
+# P can also concat the strings, or format, or f'{}'
