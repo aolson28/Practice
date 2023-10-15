32650064d11551054e5f30f336a7bdac5f76d324
aolson28 <aaronmichael.olson@gmail.com>
2023-10-14 21:18:35 -0400

    Update 10.14.23 use math operator given as a string.py

--- 10.14.23 use math operator given as a string.py
+++ 10.14.23 use math operator given as a string.py
@@ -2,6 +2,6 @@ def basic_op(operator, value1, value2):
     return eval(f'{value1} {operator} {value2}')
 
 # P int
-# R str where given 2 numbers and an operator, needs to do the math indicated by the operator
+# R int where given 2 numbers and an operator, needs to do the math indicated by the operator
 # E '+',3,3 -> 3 + 3 -> 6
 # P can use eval(f'{}')
