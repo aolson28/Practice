98294f97b2b74fab0098eb105ae766c582a9aa25
aolson28 <aaronmichael.olson@gmail.com>
2023-10-14 20:34:26 -0400

    Create 10.14.23 string to int.py

---
+++ 10.14.23 string to int.py
@@ -0,0 +1,7 @@
+def string_to_number(s):
+    return int(s)
+
+# P int
+# R int where the number was a string
+# E '13' -> 13
+# P int(s). May need to add some error checking for if the string is not a number
