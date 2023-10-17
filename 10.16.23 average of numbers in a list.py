ed1d54a71378840d188ad8ff45631f44ebc9127d
aolson28 <aaronmichael.olson@gmail.com>
2023-10-16 20:30:29 -0400

    Update 10.16.23 average of numbers in a list.py

--- 10.16.23 average of numbers in a list.py
+++ 10.16.23 average of numbers in a list.py
@@ -4,4 +4,4 @@ def find_average(numbers):
 # P float or int number
 # R float or int, the average of a list of numbers, 0 if list is empty
 # E [1, 3, 5] -> 3
-# P needs if len(numbers) > 0 else 0 statement to handle divide by 0 cases
+# P needs if len(numbers) > 0 else 0 statement to handle divide by 0 cases; could change if statement to if numbers else 0
