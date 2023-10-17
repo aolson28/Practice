c922e6cc836a805d61e58948634579cdf540088a
aolson28 <aaronmichael.olson@gmail.com>
2023-10-16 20:23:01 -0400

    Create 10.16.23 sum of numbers in a list.py

---
+++ 10.16.23 sum of numbers in a list.py
@@ -0,0 +1,7 @@
+def sum_array(a):
+    return sum(a)
+
+# P float or int number
+# R float or int, the sum of the numbers in a list
+# E [1, 3, 5] -> 9
+# P May need to exclude or give errors for strings? can just sum() for a list
