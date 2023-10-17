a8539bf1556009e31d61dc49eaa021c4dce54f09
aolson28 <aaronmichael.olson@gmail.com>
2023-10-16 21:44:11 -0400

    Create 10.16.23 split string to list.py

---
+++ 10.16.23 split string to list.py
@@ -0,0 +1,7 @@
+def string_to_array(s):
+    return s.split(' ')
+
+# P list
+# R list where each word is made into item in list
+# E 'This is an example' -> ['This', 'is', 'an', 'example']
+# P can use s.split(' ')
