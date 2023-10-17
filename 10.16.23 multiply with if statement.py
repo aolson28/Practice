ada1e433ee83f4c1760cbdbb14f9eff75f88367b
aolson28 <aaronmichael.olson@gmail.com>
2023-10-16 20:04:58 -0400

    Create 10.16.23 multiply with if statement.py

---
+++ 10.16.23 multiply with if statement.py
@@ -0,0 +1,7 @@
+def simple_multiplication(number) :
+    return number * 8 if number % 2 == 0 else number * 9
+
+# P int
+# R int where number * 8 if odd else number * 9
+# E 3 -> 27, 4 -> 32
+# P use single line if else statement, use % 2 == 0 to determine if even
