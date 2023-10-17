eb02dc21007edb8d2b0dcaf16ed9d63ae5ece638
aolson28 <aaronmichael.olson@gmail.com>
2023-10-17 08:23:54 -0400

    Create 10.17.23 determine third angle of triangle.py

---
+++ 10.17.23 determine third angle of triangle.py
@@ -0,0 +1,7 @@
+def other_angle(a, b):
+    return 180 - a - b
+
+# P int
+# R int of other angle of the triangle
+# E 20, 70 -> 90
+# P can subtract a & b from 180 to get last angle, sum of angles must equal 180 for a triangle
