5306071ba9aa68af5c8d26de0086fd612ce1528b
aolson28 <aaronmichael.olson@gmail.com>
2023-10-19 20:50:16 -0400

    Create 10.19.23 is the number square.py

---
+++ 10.19.23 is the number square.py
@@ -0,0 +1,7 @@
+def is_square(n):    
+    return n >= 0 and n**0.5 % 1 == 0
+
+# P bool
+# R bool true if n is a square number
+# E 8 -> false, 9 -> true
+# P Has to be positive number and have a square root where the result is a int()
