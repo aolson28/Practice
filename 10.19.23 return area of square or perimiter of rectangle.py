af83ece8c18d2a72de7d80d3ac661ea55d4c8e21
aolson28 <aaronmichael.olson@gmail.com>
2023-10-19 20:33:15 -0400

    Create 10.19.23 return area of square or perimiter of rectangle.py

---
+++ 10.19.23 return area of square or perimiter of rectangle.py
@@ -0,0 +1,10 @@
+def area_or_perimeter(l , w):
+    if l == w:
+        return l * w
+    else:
+        return (l * 2) + (w * 2)
+
+# P int
+# R int of area of square or perimeter of rectangle
+# E 5,5 -> 25 , 3,7 -> 20
+# P square is l == w
