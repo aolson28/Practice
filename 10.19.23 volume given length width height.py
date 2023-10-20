81d33b1a7101d938f6988b87a0cc100f3eb55fa5
aolson28 <aaronmichael.olson@gmail.com>
2023-10-19 20:29:23 -0400

    Create 10.19.23 volume given length width height.py

---
+++ 10.19.23 volume given length width height.py
@@ -0,0 +1,7 @@
+def get_volume_of_cuboid(length, width, height):
+    return length * width * height
+
+# P int
+# R int of volume of cuboid given length, width, and height
+# E 2,3,4 -> 24
+# P volume of cuboid is length * width * height
