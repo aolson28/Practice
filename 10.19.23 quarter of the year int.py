ef7a6df4d70b031a8b29814a58c708fec5706a63
aolson28 <aaronmichael.olson@gmail.com>
2023-10-19 20:09:42 -0400

    Create 10.19.23 quarter of the year int.py

---
+++ 10.19.23 quarter of the year int.py
@@ -0,0 +1,7 @@
+def quarter_of(month):
+    return int((month + 2)/3)
+
+# P int
+# R int representing what quarter of the year the month is in
+# E 4 -> 2
+# P add 2 months and divide by 3, int() will round down to nearest whole digit
