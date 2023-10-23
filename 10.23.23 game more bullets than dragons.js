2cd1708301240cf600f0620b6daf39b8ca84671c
aolson28 <aaronmichael.olson@gmail.com>
2023-10-23 14:56:19 -0400

    Create 10.23.23 game more bullets than dragons.js

---
+++ 10.23.23 game more bullets than dragons.js
@@ -0,0 +1,8 @@
+function hero(bullets, dragons){
+    return bullets >= dragons * 2;
+  }
+
+// P bool
+// R bool if the character has enough bullets (2/dragon) to defeat all the dragons
+// E 7, 3 = true 5, 3 = false
+// P bullets must be >= dragons * 2
