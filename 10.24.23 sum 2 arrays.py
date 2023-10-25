bf12a739a1effe03207a13d91b8668935fd19d6f
aolson28 <aaronmichael.olson@gmail.com>
2023-10-24 20:02:52 -0400

    Create 10.24.23 sum 2 arrays.py

---
+++ 10.24.23 sum 2 arrays.py
@@ -0,0 +1,7 @@
+def array_plus_array(arr1,arr2):
+    return sum(arr1) + sum(arr2)
+
+# P int
+# R int where 2 lists are summed and added together
+# E [1,2,3], [4,5,6] -> 21
+# P summ and add the arrays
