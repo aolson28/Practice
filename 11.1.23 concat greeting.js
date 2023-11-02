1a332aad763443cc1a099992a33a51c838322ca9
aolson28 <aaronmichael.olson@gmail.com>
2023-11-01 21:23:55 -0400

    Create 11.1.23 concat greeting.js

---
+++ 11.1.23 concat greeting.js
@@ -0,0 +1,8 @@
+function greet(name){
+    return 'Hello, ' + name + ' how are you doing today?'
+  }
+
+// P string
+// R string greeting with the argument included in the string
+// E 'Jimbo' -> 'Hello, Jimbo how are you doing today?'
+// P can use + to concat strings
