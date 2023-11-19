5e29973076d0672ced9ad37a72ed95c6d498f69f
aolson28 <aaronmichael.olson@gmail.com>
2023-11-19 14:16:02 -0500

    Create 11.19.23 reversed sentence order.js

---
+++ 11.19.23 reversed sentence order.js
@@ -0,0 +1,9 @@
+function reverseWords(str){
+    return str.split(' ').reverse().join(' ');
+  }
+  
+
+// P str
+// R str where the words are in reverse order
+// E 'Try this!' -> 'this! Try'
+// P can split to array, reverse, and then join
