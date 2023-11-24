69f53b3468abe6bce7b53b6282ba2554adc83d17
aolson28 <aaronmichael.olson@gmail.com>
2023-11-24 17:44:03 -0500

    Create 11.24.23 average array.js

---
+++ 11.24.23 average array.js
@@ -0,0 +1,12 @@
+var findAverage = function (nums) {
+    let sum =0;
+    nums.frEach( num => {
+        sum = num += sum
+        }
+    )
+    return sum/nums.length
+}
+
+// P int
+// R int which is average of numbers in an array
+// E [2,5,8] = 5
