1b3891fe519613e6c29c80160cf2acc6a7ac573e
aolson28 <aaronmichael.olson@gmail.com>
2023-12-16 22:54:49 -0500

    Create 12.16.23 case switch function.js

---
+++ 12.16.23 case switch function.js
@@ -0,0 +1,18 @@
+function caseInSwitch(val) {
+    let answer = "";
+    switch (val) {
+      case 1:
+        answer = "Snow Leopard";
+        break;
+      case 2:
+        answer = "Red Panda";
+        break;
+      case 3:
+        answer = "Girraffe";
+        break;
+      case 4:
+        answer = "Cat";
+        break;
+    }
+    return answer;
+  }
