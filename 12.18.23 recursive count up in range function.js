bd8f5f7fb6906da4271be42dade1dc4e883e06ad
aolson28 <aaronmichael.olson@gmail.com>
2023-12-21 19:46:58 -0500

    Update 12.18.23 recursive count up in range function.js

--- 12.18.23 recursive count up in range function.js
+++ 12.18.23 recursive count up in range function.js
@@ -1,14 +1,14 @@
 function rangeOfNumbers(startNum, endNum) {
-  if (endNum < startNum) {
-    return [];
-  } else {
-    const countArray = rangeOfNumbers(startNum, endNum-1);
-    countArray.push(endNum);
-    return countArray;
-  }
-};
-
-
-console.log(rangeOfNumbers(1,5));
-
-// recursive for returning an array of numbers counting up in the given start and end range
+    if (endNum < startNum) {
+      return [];
+    } else {
+      const countArray = rangeOfNumbers(startNum, endNum-1);
+      countArray.push(endNum);
+      return countArray;
+    }
+  };
+  
+  
+  console.log(rangeOfNumbers(1,5));
+  
+  // recursive for returning an array of numbers counting up in the given start and end range
